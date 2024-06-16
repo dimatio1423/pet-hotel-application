a076015c576a4296a8882033eaeb55cde2b6e540
Fu <phunnse171674@fpt.edu.vn>
2024-06-16 21:01:10 +0700

    [feat] view pet care services

---
+++ PetHotelApplication/PetHotelApplicationRazorPage/Pages/Pet/Services.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using BusinessObjects.Entities;
+using Services.Services.PetCareServices;
+using Repositories;
+
+namespace PetHotelApplicationRazorPage.Pages.Pet
+{
+    public class ServicesModel : PageModel
+    {
+        private readonly IPetCareService _petCareService;
+
+        public ServicesModel(IPetCareService petCareService)
+        {
+            _petCareService = petCareService;
+        }
+
+        public IList<PetCareService> PetCareService { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            PetCareService = _petCareService.GetPetCareServices();
+        }
+    }
+}
