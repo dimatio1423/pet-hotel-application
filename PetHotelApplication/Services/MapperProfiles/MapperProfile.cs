747bce8a8333cca58111fd9df31bd29984d1a025
Fu <phunnse171674@fpt.edu.vn>
2024-06-16 21:24:39 +0700

    [update] add auto mapper

---
+++ PetHotelApplication/Services/MapperProfiles/MapperProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using BusinessObjects.Entities;
+using BusinessObjects.Models.PetCareModel.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.MapperProfiles
+{
+    public class MapperProfile : Profile
+    {
+        public MapperProfile()
+        {
+            CreateMap<PetCareService, PetCareResModel>();
+        }
+    }
+}
